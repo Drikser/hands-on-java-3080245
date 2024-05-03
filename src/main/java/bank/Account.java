b34b8fe6ac72bb1f397b54bcfae832bac30475e5
Drikser <50340267+Drikser@users.noreply.github.com>
2024-05-03 09:14:56 +0000

    Account Class creation

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,11 @@
+package bank;
+
+public class Account {
+
+  private int id; // private = can't be accessed outside of this class (=encapsulation)
+  private String name;
+  private String username;
+  private String password;
+  private int accountId;
+
+}
