813d506bab075067f2272f797d0012d5ecfc3e8a
Drikser <50340267+Drikser@users.noreply.github.com>
2024-05-03 09:15:45 +0000

    Account Class creation

--- src/main/java/bank/Account.java
+++ src/main/java/bank/Account.java
@@ -3,9 +3,40 @@ package bank;
 public class Account {
 
   private int id; // private = can't be accessed outside of this class (=encapsulation)
-  private String name;
-  private String username;
-  private String password;
-  private int accountId;
+  private String type;
+  private double balance;
+
+  // constructor
+  public Account(int id, String type, double balance) {
+    // And in the constructor, we call the set methods
+    setId(id);
+    setType(type);
+    setBalance(balance);
+  }
+
+  // getters and setters
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
 
 }
